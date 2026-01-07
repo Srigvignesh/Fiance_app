6870a93031f8b11a8161ff71bf36a0630f893d73
Sri G Vignesh <71248441+Srigvignesh@users.noreply.github.com>
2026-01-07 22:14:57 +0530

    Delete LedgerApp directory

--- LedgerApp/Year/year.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import shutil
-from pathlib import Path
-
-def create_year_archive():
-    if Path("LedgerApp").exists():
-        shutil.make_archive("LedgerApp_Full", 'zip', "LedgerApp")
-        return Path("LedgerApp_Full.zip")
-    return None
